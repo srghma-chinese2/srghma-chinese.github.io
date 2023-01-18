e2616e0ef2ae4a34c9a696e5143373c4a6e54a62
Serhii Khoma <srghma@gmail.com>
2023-01-18 13:41:38 +0200

    feat: update

--- make-pleco-user-dictionary.js
+++ make-pleco-user-dictionary.js
@@ -18,7 +18,7 @@ const removeLinks = x => x.replace(/<link>[^<]*<\/link>/g, '')
 function ruPinyinTextToArray(text) {
   text = text.replace(/\t/g, '').split(/―{4,}|-{4,}/)
   text = text.map(x => x.split('\n').map(x => x.trim()).join('\n'))
-  text = text.map(x => x.split(/_{3,}/).map(x => x.trim()).join(`\n\n______________\n\n`).trim())
+  text = text.map(x => x.split(/_{3,}/).map(x => x.trim()).filter(x => x).join(`\n\n______________\n\n`).trim())
   text = text.filter(x => x)
   return text
 }
